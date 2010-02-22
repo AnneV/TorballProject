15de8a200d09af10c7f19a0fe0a919d7025e4a34
nelle@EmpressOfIreland <nelle.varoquaux@gmail.com>
2010-02-22 18:35:44 +0100

    generation du controller et model page

--- torball/db/schema.rb
+++ torball/db/schema.rb
@@ -9,7 +9,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100222152307) do
+ActiveRecord::Schema.define(:version => 20100222171411) do
+
+  create_table "pages", :force => true do |t|
+    t.string   "title"
+    t.boolean  "published"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
