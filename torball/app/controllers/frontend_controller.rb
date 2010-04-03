03c904044e0857bbfdf4e50b837510959209a60c
nelle@EmpressOfIreland <nelle.varoquaux@gmail.com>
2010-04-03 14:20:53 +0200

    news et edito mnt affiché sur la page d'accueil

--- torball/app/controllers/frontend_controller.rb
+++ torball/app/controllers/frontend_controller.rb
@@ -5,10 +5,25 @@ class FrontendController < ApplicationController
   def home
     @page = Page.find_by_home(true)
     @menus = Menu.all(:conditions => {:published => true})
+    cat_news = Category.find_by_name('News')
+    cat_editorial = Category.find_by_name('Editorial')
+    @news = Post.all(:conditions => {:category_id => cat_news.id, 
+                                     :published => true}, 
+                    :order => :created_at,
+                    :limit => 3)
+    @editos = Post.all(:conditions => {:category_id => cat_editorial.id, 
+                                     :published => true}, 
+                    :order => :created_at,
+                    :limit => 3)
   end
 
   def show
     @page = Page.find_by_id(params[:id])
+    @menus = Menu.all(:conditions => {:published => true})
+  end
+
+  def edito
+    @post = Post.find_by_id(params[:id])
     @menus = Menu.all(:conditions => {:published => true})
   end
 
